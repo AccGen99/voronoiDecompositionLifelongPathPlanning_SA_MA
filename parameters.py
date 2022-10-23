2474d8d64dbbc2a8ff9d84504aeb7bd96e0b630c
AccGen99 <72756584+AccGen99@users.noreply.github.com>
2022-10-23 21:06:13 +0530

    semi-final commit

---
+++ parameters.py
@@ -0,0 +1,16 @@
+NUM_AGENTS = 7
+NUM_TARGETS = 250
+INITIAL_ENERGY = 2.5
+
+C_TYPE = 'None'
+
+RES = 1000 # Grid resolution
+
+# Force_balance constants
+STD_DEV_TOL = 0.025
+ITERS = 1000
+
+DEL_T = 0.5         # Centre & Velocity
+LAMDA = 0.8         # Velocity 
+BETA = 100            # Velocity
+ALPHA = 0.04         # Force
