c6455df656b37c6cc5c811b663f65ef38470cade
AccGen99 <72756584+AccGen99@users.noreply.github.com>
2022-10-23 15:42:04 +0530

    additional parameters for ma

--- parameters.py
+++ parameters.py
@@ -1,7 +1,16 @@
-NUM_AGENTS = 1
-NUM_TARGETS = 10
-INITIAL_ENERGY = 10
+NUM_AGENTS = 4
+NUM_TARGETS = 250
+INITIAL_ENERGY = 10 / NUM_AGENTS
 
 C_TYPE = 'None'
 
-RES = 100 # Grid resolution
+RES = 1000 # Grid resolution
+
+# Force_balance constants
+STD_DEV_TOL = 0.1
+ITERS = 100
+
+DEL_T = 0.5         # Centre & Velocity
+LAMDA = 1           # Velocity 
+BETA = 1            # Velocity
+ALPHA = 0.04        # Force
